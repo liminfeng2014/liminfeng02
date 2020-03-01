4b669689b7c7fed047fa87f2befcb5404807c116
liminfeng2014 <84263763@qq.com>
2020-03-01 22:37:48 +0800

    masterupdate

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-	System.out.println("idea test liminfeng -merger liminfeng");
+	System.out.println("idea test liminfeng -merger liminfeng master");
 
     }
 }
